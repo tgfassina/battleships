54982723ae07c8ba531e43787dc85859c6f543f7
Thiago Fassina <thiagoaaa@gmail.com>
2017-07-11 13:39:17 -0300

    basic test scaffolding

---
+++ test/hello-spec.js
@@ -0,0 +1,5 @@
+describe('Hello', function() {
+	it('should greet', function() {
+		expect('hello').to.equal('hello');
+	});
+});
---
+++ test/support/mocha.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var chai = require('chai');
+
+chai.should();
+
+global.expect = chai.expect;
+global.assert = chai.assert;
+global.fail = chai.assert.fail;
