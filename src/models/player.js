74976bd437407c106d4141322b13db4db595b784
Thiago Fassina <thiagoaaa@gmail.com>
2017-07-11 15:06:08 -0300

    player signup generates a guid

--- src/models/player.js
+++ src/models/player.js
@@ -1,8 +1,11 @@
 var Player = function() {
+
+	var Guid = require('guid');
+
 	var api = {};
 
 	api.signUp = function() {
-		return '00000000-0000-0000-0000-000000000000';
+		return Guid.raw();
 	};
 
 	return api;
--- test/player-spec.js
+++ test/player-spec.js
@@ -11,9 +11,15 @@ describe('Player', function() {
 	describe('signup', function() {
 
 		it('should provide guid', function() {
+			var guid = player.signUp();
+			expect(guid.length).to.equal(36);
+		});
 
-			var identity = player.signUp();
-			expect(identity.length).to.equal(36);
+		it('should provide a unique guid', function() {
+			var guid1 = player.signUp();
+			var guid2 = player.signUp();
+
+			expect(guid1).to.not.equal(guid2);
 		});
 	});
 });
