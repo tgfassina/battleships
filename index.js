3ef798edd5f5f4ebe466c6ada4c5848a0219a2b6
Thiago Fassina <thiagoaaa@gmail.com>
2017-07-11 16:02:34 -0300

    api proof of concept

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+var PlayerApi = require('./src/apis/player.js');
+PlayerApi(app);
+
+
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).send('You broke it!');
+});
+
+
+app.listen(3000, function () {
+	console.log('Battleship operational!')
+})
