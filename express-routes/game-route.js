25eb8657557fc41419eb5daa37e2a8728817ffcf
Thiago Fassina <thiagoaaa@gmail.com>
2017-07-13 18:13:49 -0300

    added game/ready route

--- express-routes/game-route.js
+++ express-routes/game-route.js
@@ -11,6 +11,18 @@ GameRoute = function(app, game) {
 				res.status(500).send(msg);
 			});
 	});
+
+	app.post('/game/ready', function (req, res) {
+		game.ready(req.body.guid, req.body.gameId)
+			.then(function(shipName) {
+				res.send({
+					message: 'You are ready'
+				});
+			})
+			.catch(function(msg) {
+				res.status(500).send(msg);
+			});
+	});
 };
 
 module.exports = GameRoute;
